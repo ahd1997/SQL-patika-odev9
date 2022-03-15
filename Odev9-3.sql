22fd5c58944de950c5e38b4061d9cf5e488dbde5
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-15 21:43:19 +0300

    Update Odev9-3.sql

--- Odev9-3.sql
+++ Odev9-3.sql
@@ -1,6 +1,6 @@
 --dvdrental örnek veri tabanı üzerinden customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki 
 --first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
-SELECT city,country
+SELECT rental_id, first_name, last_name
 FROM customer
 INNER JOIN rental
-ON customer. = rental. ;
+ON customer.customer_id = rental.rental_id ;
