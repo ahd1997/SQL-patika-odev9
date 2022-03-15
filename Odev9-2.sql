0554e4731a41c1033f2cdd7177832b73491d0a08
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-15 21:41:34 +0300

    Update Odev9-2.sql

--- Odev9-2.sql
+++ Odev9-2.sql
@@ -1,7 +1,6 @@
 --dvdrental örnek veri tabanı üzerinden customer tablosu ile payment tablosunda bulunan 
 --payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
-SELECT first_name,last_name 
+SELECT payment_id,first_name,last_name 
 FROM customer
 INNER JOIN payment
-ON customer. = payment. 
-CONCAT(first_name ' ' last_name);
+ON customer.customer_id = payment. customer_id ;
