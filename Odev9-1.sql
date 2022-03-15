f9b7617a0c3b10a37c9aea9544dbb48351eae2ee
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-15 21:33:34 +0300

    Create Odev9-1.sql

---
+++ Odev9-1.sql
@@ -0,0 +1,5 @@
+--dvdrental örnek veri tabanı üzerinden city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city,country
+FROM city
+INNER JOIN country
+ON city. = country. ;
