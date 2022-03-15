df7fe9fbd658d289373496fd6a810d4cb558cd7b
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-15 21:33:29 +0300

    Create Odev9-3.sql

---
+++ Odev9-3.sql
@@ -0,0 +1,6 @@
+--dvdrental örnek veri tabanı üzerinden customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki 
+--first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city,country
+FROM customer
+INNER JOIN rental
+ON customer. = rental. ;
